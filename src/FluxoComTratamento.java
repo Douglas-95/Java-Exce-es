1d54fd3fe54dbf9efd51a44d784c3736fd969cca
Douglas Crepes <douglascrepes@gmail.com>
2024-01-10 19:51:25 -0300

    Lançando Exceções

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -11,7 +11,6 @@ public class Fluxo {
         }
         
         System.out.println("Fim do main");
-        metodo3();
     }
 
     private static void metodo1() {
@@ -22,26 +21,9 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            int a = i / 0;
-            //Conta c = null;
-            //c.deposita();
-        }
-        System.out.println("Fim do metodo2");
+        
+        throw new ArithmeticException("deu errado");
+        
+        //System.out.println("Fim do metodo2");
     }
-
-    private static void metodo3(){
-        System.out.println("Ini metodo3 teste");
-                for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            //int a = i / 0;
-            Conta c = null;
-            c.deposita();
-        }
-        System.out.println("Fim do metodo3");
-    }
-    
-
-    
 }
